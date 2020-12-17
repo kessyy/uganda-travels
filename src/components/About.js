ccd526336443b3099e694c9c28c85c838e54306b
kessyy <kather946@gmail.com>
2020-12-17 12:11:06 +0300

    services component

--- src/components/About.js
+++ src/components/About.js
@@ -1,11 +1,6 @@
 import React, { Component } from "react";
-import { Container, Row, Col } from "reactstrap";
+// import { Container, Row, Col } from "reactstrap";
 import "../App.css";
-import MainCarousel from "./Carousel";
-// import { ReactComponent as Award } from "../assets/svg/award.svg";
-// import { ReactComponent as Shield } from "../assets/svg/shield.svg";
-// import { ReactComponent as Compass } from "../assets/svg/compass.svg";
-// import { ReactComponent as Heart } from "../assets/svg/heart.svg";
 
 class About extends Component {
   render() {
